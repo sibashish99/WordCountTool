62d89217f17c197888f2ec1f970f33049c20da8d
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-05-14 21:45:31 +0530

    Update wordCount.java

--- src/CountingWords/wordCount.java
+++ src/CountingWords/wordCount.java
@@ -5,10 +5,7 @@
  */
 package CountingWords;
 
-/**
- *
- * @author Bluelime Learning So
- */
+
 public class wordCount extends javax.swing.JFrame {
 
     /**
@@ -174,17 +171,9 @@ public class wordCount extends javax.swing.JFrame {
         }
           totalCountTextField.setText(" "+words);
         
-    }//GEN-LAST:event_wrdCountButtonActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
+    }
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+       
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -201,9 +190,7 @@ public class wordCount extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(wordCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
+       
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new wordCount().setVisible(true);
